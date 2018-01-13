bb8116896adcee17ac49d7e80befc2d8c392cd5f
tirtha <tsark99@gmail.com>
2018-01-13 12:02:34 +0530

    Adding files

---
+++ vue/app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#app',
+
+  data: {
+      Name: "Tirtha Sarker",
+      Role: "Developer",
+      image: "https://goo.gl/UufH54",
+      quote: 'If you want to succeed as bad as you want to breathe, then you will be successful',
+      facebook: "https://www.facebook.com/tirtha.sarker",
+      twitter: "https://twitter.com/tsark99",
+      linkedin: "https://www.linkedin.com/in/tsarker/",
+
+
+  },
+
+
+})
